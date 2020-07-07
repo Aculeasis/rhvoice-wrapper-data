1c6b7fe235b8cbc295808b54024642d4c3975540
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2020-07-07 20:10:10 +0300

    Get version from git tags, remove file version

--- setup.py
+++ setup.py
@@ -77,15 +77,41 @@ class RHVoiceBuild(build):
         build.run(self)
 
 
-with open('README.md') as fh:
-    long_description = fh.read()
+def get_version() -> str:
+    version_file = 'version'
 
-with open('version') as fh:
-    version = fh.read().splitlines()[0]
+    def version_to_file(ver):
+        with open(version_file, mode='w') as fd:
+            fd.write(ver)
+
+    def version_from_file():
+        with open(version_file) as fd:
+            return fd.read().splitlines()[0]
+
+    def version_from_git():
+        cmd = ['git', 'describe', '--abbrev=0', '--tags']
+        try:
+            return subprocess.check_output(cmd).decode().splitlines()[0]
+        except Exception as e:
+            print('ERROR! Execute {}: {}'.format(cmd, e))
+            return None
+    version = version_from_git()
+    if not version:
+        version = version_from_file()
+        print('WARNING! Get version from a file: {}'.format(version))
+    else:
+        version_to_file(version)
+    return version
+
+
+def get_long_description():
+    with open('README.md') as fh:
+        return fh.read()
+
 
 setup(
     name='rhvoice-wrapper-data',
-    version=version,
+    version=get_version(),
     packages=[PACKAGE_PATH],
     package_data={PACKAGE_PATH: [os.path.join(DATA_DIR, '*')]},
     url='https://github.com/Aculeasis/rhvoice-wrapper-data',
@@ -93,7 +119,7 @@ setup(
     author='Aculeasis',
     author_email='amilpalimov2@ya.ru',
     description='Provides RHVoice data for rhvoice-wrapper-bin',
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type='text/markdown',
     python_requires='>=3.4',
     classifiers=[
